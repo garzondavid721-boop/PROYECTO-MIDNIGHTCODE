e4f7ffa7fc267d32e73e3d7828ee9741226679c7
STDADeath <Carrimorro9080@gmail.com>
2026-03-18 15:39:33 -0500

    featu:test

---
+++ midnightcode/src/server.js
@@ -0,0 +1,103 @@
+/* ---------------- DEPENDENCIAS ---------------- */
+
+const http = require("http");
+const { Server } = require("socket.io");
+const cron = require("node-cron");
+
+/* ---------------- LOGGER ---------------- */
+
+const logger = require("./config/logger");
+
+/* ---------------- JOBS ---------------- */
+
+require("./jobs/liberarReservasJob");
+require("./jobs/limpiarCanciones");
+
+const liberarReservas = require("./jobs/reservaExpirationJob");
+
+/* ---------------- APP ---------------- */
+
+const app = require("./app");
+
+/* ---------------- SERVER ---------------- */
+
+const server = http.createServer(app);
+
+/* ---------------- SOCKET.IO ---------------- */
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+global.io = io;
+
+io.on("connection", (socket) => {
+
+  logger.info({
+    event: "SOCKET_CONECTADO",
+    socket: socket.id
+  });
+
+  socket.on("disconnect", () => {
+
+    logger.info({
+      event: "SOCKET_DESCONECTADO",
+      socket: socket.id
+    });
+
+  });
+
+});
+
+/* ---------------- CRON JOB ---------------- */
+
+/* revisa reservas expiradas cada minuto */
+
+cron.schedule("* * * * *", async () => {
+
+  try {
+
+    logger.info({
+      event: "CRON_VERIFICAR_RESERVAS"
+    });
+
+    await liberarReservas();
+
+  } catch (error) {
+
+    logger.error({
+      event: "ERROR_CRON_RESERVAS",
+      error: error.message
+    });
+
+  }
+
+});
+
+/* ---------------- START SERVER ---------------- */
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "test") {
+  server.listen(PORT, () => {
+    logger.info({
+      event: "SERVER_START",
+      port: PORT
+    });
+  }).on("error", (err) => {
+
+    if (err.code === "EADDRINUSE") {
+
+      logger.error({
+        event: "PUERTO_OCUPADO",
+        port: PORT
+      });
+
+      process.exit(1);
+
+    }
+
+  });
+}
