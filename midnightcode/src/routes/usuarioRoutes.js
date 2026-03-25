f9a053a524941b2560673a4bc283de978c30559b
Fernanda Benavides <benavidesmariafer1825@gmail.com>
2026-03-25 17:10:30 -0500

    featu:fUNCIONA JEST

--- midnightcode/src/routes/usuarioRoutes.js
+++ midnightcode/src/routes/usuarioRoutes.js
@@ -8,28 +8,23 @@ const rolMiddleware = require("../middlewares/roleMiddleware");
 const ownerOrAdmin = require("../middlewares/ownerOrAdmin");
 
 
-// REGISTRO Y LOGIN
+
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 
 
-// VER TODOS LOS USUARIOS (SOLO ADMIN)
 router.get("/", authMiddleware, rolMiddleware([1]), controller.getAll);
 
 
-// VER USUARIO POR ID (ADMIN O EL MISMO USUARIO)
 router.get("/:id", authMiddleware, ownerOrAdmin, controller.getById);
 
 
-// ACTUALIZAR USUARIO (SOLO ADMIN)
 router.put("/:id", authMiddleware, rolMiddleware([1]), controller.update);
 
 
-// MODIFICAR PARCIAL (SOLO ADMIN)
 router.patch("/:id", authMiddleware, rolMiddleware([1]), controller.patch);
 
 
-// ELIMINAR USUARIO (SOLO ADMIN)
 router.delete("/:id", authMiddleware, rolMiddleware([1]), controller.delete);
 
 
